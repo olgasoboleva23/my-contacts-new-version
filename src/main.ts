7fc612227dd6c1b89ccf709f9bb41fbd2cfc59ce
olgasoboleva1998 <lelya.soboleva.1998@mail.ru>
2022-08-14 23:04:29 +0300

    added quasar

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
 
-const app = createApp(App)
+const app = createApp(App).use(Quasar, quasarUserOptions)
 
 app.use(createPinia())
 app.use(router)
