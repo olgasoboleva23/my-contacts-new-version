b7aea4855d792fb3424a47908eaab3c17636b179
olgasoboleva1998 <lelya.soboleva.1998@mail.ru>
2022-08-14 23:03:38 +0300

    added component and icons

---
+++ src/components/ContactsTable.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+<table>
+<tr v-for="item in cites" :key="item.id">
+<td>{{ item.citeName }}</td>
+<td><q-btn :icon="`img:${item.img}`" @click="goTo(item.url)">
+</q-btn></td>
+</tr></table></div>
+</template>
+
+<script>
+export default {
+  name: 'ContactsTable',
+  data() {
+    return {
+      cites: [
+	{				
+	id: '1',
+	citeName: 'GitHub',
+	url: 'https://github.com/olgasoboleva23',
+	img: 'GitHub.svg'
+	},
+	{
+	id: '2',
+	citeName: 'VK',
+	url: 'https://vk.com/id483703213',
+	img: 'vk.svg'
+	},
+	{
+	id: '3',
+	citeName: 'telegram',
+	url: 'https://t.me/@helga_soboleva',
+	img: 'telegram.svg'
+	},
+      ]
+    }
+  },
+  methods: {
+    goTo(url) {
+      window.location.href=url
+    }
+  }
+}
+</script>
